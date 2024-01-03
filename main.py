d787bdf5ede5a6697792b8e8e273a1ff285b7fe3
linda <linda.alhasanalnsour@postgrad.manchester.ac.uk>
2024-01-03 15:52:36 +0000

    difference between lists for loops and comprehensions

--- main.py
+++ main.py
@@ -169,6 +169,47 @@ newlist = [x for x in fruits if "a" in x]
 
 print(newlist)
 
+
+# Sort lists:
+#Sort the list based on how close the number is to 50:
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+#Sort the list alphabetically: / Sort the list numerically
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+
+# Sort the list descending:
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+
+#Perform a case-insensitive sort of the list:
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+
+#Reverse the order of the list items:
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+
+
+
 newlist = [x for x in fruits if x != "apple"]
 
 
